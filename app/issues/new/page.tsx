27eb70e7428f992d252a267f5f212e740d65aadf
Gift <Giftibe62@gmail.com>
2024-02-16 14:28:48 +0100

    feat: implemented client-side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,20 +1,23 @@
 'use client'
-import { Button, Callout, TextField } from '@radix-ui/themes'
+import { Button, Callout, Text, TextField } from '@radix-ui/themes'
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css"
 import { useForm, Controller } from 'react-hook-form'
 import axios from 'axios'
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod'
+import { createIssueSchema } from '@/app/validationSchema';
+import { z } from 'zod'
+// import {  } from 'react-hook-form';
 
-interface IssueForm {
-    title: string,
-    description: string
-}
+type IssueForm = z.infer<typeof createIssueSchema>
 
 const NewIssuePage = () => {
     const router = useRouter()
-    const { register, control, handleSubmit } = useForm<IssueForm>()
+    const { register, control, handleSubmit, formState: { errors } } = useForm<IssueForm>({
+        resolver: zodResolver(createIssueSchema)
+    })
     const [error, setError] = useState("")
     return (
         <div className='max-w-xl' >
@@ -43,16 +46,17 @@ const NewIssuePage = () => {
                 <TextField.Root>
                     <TextField.Input placeholder="Title" {...register('title')} />
                 </TextField.Root>
+                {errors.title && <Text color='red' as="p">{errors.title.message}</Text>}
 
-                <Controller
+                <Controller //used to aid parse the form data into a text-field
                     name="description"
                     control={control}
                     render={({ field }) => <SimpleMDE placeholder="Description" {...field} />}
                 />
+                {errors.description && <Text color='red' as="p">{errors.description.message}</Text>}
                 <Button>Submit New Issues</Button>
             </form >
         </div >
     )
 }
-
 export default NewIssuePage
